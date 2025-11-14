2259f8c30b48f8c96d5cef207c3725e4a46ea8f4
banka-jyothsna <Banka.Jyothsna@Triconinfotech.com>
2025-11-14 19:49:18 +0530

    add

---
+++ src/main/java/com/example/spring_ass/SpringAssApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_ass/SpringAssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
